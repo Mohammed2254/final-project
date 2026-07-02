091628d1b13080bab92859148d10125f35a81fad
salman_ghannam1 <sloome222@gmail.com>
2026-07-02 22:19:20 +0300

    rename user profile repo

---
+++ back end/app/repositories/user_profile_repository.py
@@ -0,0 +1,16 @@
+from app.models.user_profile import UserProfile
+from app.repositories.base_repository import BaseRepository
+
+class UserProfileRepository(BaseRepository):
+
+    def __init__(self):
+        super().__init__(UserProfile)
+
+    def get_by_account_id(self, account_id: int) -> UserProfile | None:
+        
+        return UserProfile.query.filter_by(
+            account_id=account_id
+        ).ne_or_none()
+
+    def exists(self, account_id: int) -> bool:
+        return self.get_by_account_id(account_id) is not None
