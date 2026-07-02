e29e5a2efbd8ea82c634509b19d3956b4039956c
salman_ghannam1 <sloome222@gmail.com>
2026-07-02 19:55:08 +0300

    extensions

--- back end/app/extensions.py
+++ back end/app/extensions.py
@@ -1,22 +1,12 @@
-"""
-extensions.py
-
-Shared Flask extensions used throughout the application.
-"""
-
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 
-# Password Hashing
 bcrypt = Bcrypt()
 
-# Database ORM
 db = SQLAlchemy()
 
-# Database Migration
 migrate = Migrate()
 
-# JWT Authentication
 jwt = JWTManager()
