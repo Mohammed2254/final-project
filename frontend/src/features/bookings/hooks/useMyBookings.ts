c3e97a4e1720422eeea17d70409972e3712bc973
Mohammed2254 <m.a.alabduli@gmail.com>
2026-07-23 03:50:26 +0300

    chore: remove dead @ folder created by shadcn (unused duplicate of src/components/ui/button)

--- frontend/src/features/bookings/hooks/useMyBookings.ts
+++ frontend/src/features/bookings/hooks/useMyBookings.ts
@@ -28,6 +28,7 @@ export function useMyBookings() {
   }, []);
 
   useEffect(() => {
+    // eslint-disable-next-line react-hooks/set-state-in-effect
     void fetchBookings();
   }, [fetchBookings]);
 
